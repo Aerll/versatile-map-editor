8a8fb15920300165ff6a0c63ae9e631d23499527
Aerll <31746984+Aerll@users.noreply.github.com>
2021-09-05 13:19:57 +0200

    tile classes

--- DDNetLib/include/ddnet/map/tile.hpp
+++ DDNetLib/include/ddnet/map/tile.hpp
@@ -53,4 +53,120 @@ public:
     bool is_opaque;
 };
 
+
+
+class TeleTile {
+public:
+    inline TeleTile(const TeleTile&) = default;
+    inline TeleTile(TeleTile&&) = default;
+    inline TeleTile& operator=(const TeleTile&) = default;
+    inline TeleTile& operator=(TeleTile&&) = default;
+
+    inline TeleTile()
+        : index{ 0 }
+        , id{ 0 }
+    {
+    }
+
+    inline TeleTile(quint8 index, quint8 id)
+        : index{ index }
+        , id{ id }
+    {
+    }
+
+public:
+    quint8 index;
+    quint8 id;
+};
+
+
+
+class TuneTile {
+public:
+    inline TuneTile(const TuneTile&) = default;
+    inline TuneTile(TuneTile&&) = default;
+    inline TuneTile& operator=(const TuneTile&) = default;
+    inline TuneTile& operator=(TuneTile&&) = default;
+
+    inline TuneTile()
+        : index{ 0 }
+        , id{ 0 }
+    {
+    }
+
+    inline TuneTile(quint8 index, quint8 id)
+        : index{ index }
+        , id{ id }
+    {
+    }
+
+public:
+    quint8 index;
+    quint8 id;
+};
+
+
+
+class SpeedupTile {
+public:
+    inline SpeedupTile(const SpeedupTile&) = default;
+    inline SpeedupTile(SpeedupTile&&) = default;
+    inline SpeedupTile& operator=(const SpeedupTile&) = default;
+    inline SpeedupTile& operator=(SpeedupTile&&) = default;
+
+    inline SpeedupTile()
+        : index{ 0 }
+        , force{ 0 }
+        , max_speed{ 0 }
+        , angle{ 0 }
+    {
+    }
+
+    inline SpeedupTile(quint8 index, quint8 force, quint8 max_speed, qint16 angle)
+        : index{ index }
+        , force{ force }
+        , max_speed{ max_speed }
+        , angle{ angle }
+    {
+    }
+
+public:
+    quint8 index;
+    quint8 force;
+    quint8 max_speed;
+    qint16 angle;
+};
+
+
+
+class SwitchTile {
+public:
+    inline SwitchTile(const SwitchTile&) = default;
+    inline SwitchTile(SwitchTile&&) = default;
+    inline SwitchTile& operator=(const SwitchTile&) = default;
+    inline SwitchTile& operator=(SwitchTile&&) = default;
+
+    inline SwitchTile()
+        : index{ 0 }
+        , id{ 0 }
+        , rotation{ Rotation::N }
+        , delay{ 0 }
+    {
+    }
+
+    inline SwitchTile(quint8 index, quint8 id, Rotation rotation, quint8 delay)
+        : index{ index }
+        , id{ id }
+        , rotation{ rotation }
+        , delay{ delay }
+    {
+    }
+
+public:
+    quint8 index;
+    quint8 id;
+    Rotation rotation;
+    quint8 delay;
+};
+
 } // ddnet::map::
--- DDNetLib/source/ddnet/map/tile.cpp
+++
@@ -1,7 +0,0 @@
-#include <ddnet/map/tile.hpp>
-
-namespace ddnet::map {
-
-
-
-} // ddnet::map::
