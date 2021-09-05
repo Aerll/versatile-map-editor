3bb9ac90fd88e6ca70e29e2a2a2d3458a01b73c6
Aerll <31746984+Aerll@users.noreply.github.com>
2021-09-05 14:37:45 +0200

    quad class

--- DDNetLib/include/ddnet/map/quad.hpp
+++ DDNetLib/include/ddnet/map/quad.hpp
@@ -1,7 +1,27 @@
 #pragma once
 
+#include <QColor>
+#include <QPoint>
+
+#include <array>
+
 namespace ddnet::map {
 
+class QuadVertex {
+    QPointF position;
+    QPoint tex_coord;
+    QColor color;
+};
 
+struct Quad {
+    std::array<QuadVertex, 4> vertices;
+    QPointF pivot_position;
+
+    qint32 position_envelope_id;
+    qint32 position_envelope_offset;
+
+    qint32 color_envelope_id;
+    qint32 color_envelope_offset;
+};
 
 } // ddnet::map::
--- DDNetLib/include/ddnet/map/tile.hpp
+++ DDNetLib/include/ddnet/map/tile.hpp
@@ -25,26 +25,7 @@ ENUM_DEFINE_ALL_(Rotation);
 
 namespace ddnet::map {
 
-class Tile {
-public:
-    DEFAULT_COPY_(Tile);
-    DEFAULT_MOVE_(Tile);
-
-    inline Tile()
-        : index{ 0 }
-        , rotation{ Rotation::N }
-        , is_opaque{ false }
-    {
-    }
-
-    inline Tile(quint8 index, Rotation rotation, bool is_opaque)
-        : index{ index }
-        , rotation{ rotation }
-        , is_opaque{ is_opaque }
-    {
-    }
-
-public:
+struct Tile {
     quint8 index;
     Rotation rotation;
     bool is_opaque;
@@ -52,76 +33,21 @@ public:
 
 
 
-class TeleTile {
-public:
-    DEFAULT_COPY_(TeleTile);
-    DEFAULT_MOVE_(TeleTile);
-
-    inline TeleTile()
-        : index{ 0 }
-        , id{ 0 }
-    {
-    }
-
-    inline TeleTile(quint8 index, quint8 id)
-        : index{ index }
-        , id{ id }
-    {
-    }
-
-public:
+struct TeleTile {
     quint8 index;
     quint8 id;
 };
 
 
 
-class TuneTile {
-public:
-    DEFAULT_COPY_(TuneTile);
-    DEFAULT_MOVE_(TuneTile);
-
-    inline TuneTile()
-        : index{ 0 }
-        , id{ 0 }
-    {
-    }
-
-    inline TuneTile(quint8 index, quint8 id)
-        : index{ index }
-        , id{ id }
-    {
-    }
-
-public:
+struct TuneTile {
     quint8 index;
     quint8 id;
 };
 
 
 
-class SpeedupTile {
-public:
-    DEFAULT_COPY_(SpeedupTile);
-    DEFAULT_MOVE_(SpeedupTile);
-
-    inline SpeedupTile()
-        : index{ 0 }
-        , force{ 0 }
-        , max_speed{ 0 }
-        , angle{ 0 }
-    {
-    }
-
-    inline SpeedupTile(quint8 index, quint8 force, quint8 max_speed, qint16 angle)
-        : index{ index }
-        , force{ force }
-        , max_speed{ max_speed }
-        , angle{ angle }
-    {
-    }
-
-public:
+struct SpeedupTile {
     quint8 index;
     quint8 force;
     quint8 max_speed;
@@ -130,28 +56,7 @@ public:
 
 
 
-class SwitchTile {
-public:
-    DEFAULT_COPY_(SwitchTile);
-    DEFAULT_MOVE_(SwitchTile);
-
-    inline SwitchTile()
-        : index{ 0 }
-        , id{ 0 }
-        , rotation{ Rotation::N }
-        , delay{ 0 }
-    {
-    }
-
-    inline SwitchTile(quint8 index, quint8 id, Rotation rotation, quint8 delay)
-        : index{ index }
-        , id{ id }
-        , rotation{ rotation }
-        , delay{ delay }
-    {
-    }
-
-public:
+struct SwitchTile {
     quint8 index;
     quint8 id;
     Rotation rotation;
