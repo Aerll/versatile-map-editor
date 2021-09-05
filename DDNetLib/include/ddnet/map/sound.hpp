dafcd21b61a475d101f3fd02393891e77874dc50
Aerll <31746984+Aerll@users.noreply.github.com>
2021-09-05 14:45:56 +0200

    sound source classes

--- DDNetLib/include/ddnet/map/sound.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-namespace ddnet::map {
-
-
-
-} // ddnet::map::
---
+++ DDNetLib/include/ddnet/map/sound_source.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <QPoint>
+#include <QSize>
+
+namespace ddnet::map {
+
+struct SoundSource {
+    QPointF position;
+
+    qint32 delay_seconds;
+    qint32 falloff; // from 0 to 255
+
+    qint32 position_envelope_id;
+    qint32 position_envelope_offset;
+
+    qint32 sound_envelope_id;
+    qint32 sound_envelope_offset;
+
+    bool is_looped;
+    bool has_panning;
+
+};
+
+struct SoundSourceCircle : SoundSource {
+    qint32 radius;
+};
+
+struct SoundSourceRectangle : SoundSource {
+    QSize size;
+};
+
+} // ddnet::map::
