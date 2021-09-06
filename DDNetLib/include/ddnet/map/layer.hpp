7cc72817fda116512db606e79ebab9e37fe0f4e8
Aerll <31746984+Aerll@users.noreply.github.com>
2021-09-06 17:03:24 +0200

    classes for special layers

--- DDNetLib/include/ddnet/map/layer.hpp
+++ DDNetLib/include/ddnet/map/layer.hpp
@@ -6,6 +6,30 @@ class TileLayer {
     // TODO
 };
 
+class GameLayer : public TileLayer {
+    // TODO
+};
+
+class FrontLayer : public TileLayer {
+    // TODO
+};
+
+class TeleLayer : public TileLayer {
+    // TODO
+};
+
+class TuneLayer : public TileLayer {
+    // TODO
+};
+
+class SwitchLayer : public TileLayer {
+    // TODO
+};
+
+class SpeedupLayer : public TileLayer {
+    // TODO
+};
+
 class QuadLayer {
     // TODO
 };
