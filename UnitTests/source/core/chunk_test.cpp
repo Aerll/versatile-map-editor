619db92f7eb42d742a69dee1b403f4aa128e253b
Aerll <31746984+Aerll@users.noreply.github.com>
2022-07-13 20:08:18 +0200

    add basic chunk functionality

--- UnitTests/source/core/chunk_test.cpp
+++ UnitTests/source/core/chunk_test.cpp
@@ -6,15 +6,50 @@ using namespace vt;
 
 TEST_CASE("core::Chunk") {
     SECTION("Initialize from position") {
-        quint16 x = 7;
-        quint16 y = 37;
-        core::Chunk<qint32> chunk{ x, y };
+        QPoint pos = { 7, 37 };
+        core::Chunk<qint32> chunk{ pos };
 
-        CHECK(chunk.getID() == static_cast<quint32>(y * constants::_grid_size + x));
+        CHECK(chunk.getID() == static_cast<quint32>(pos.y() * constants::_grid_size + pos.x()));
 
         REQUIRE(chunk.tiles.size() == constants::_chunk_size * constants::_chunk_size);
         
         chunk.tiles[3 * constants::_chunk_size + 7] = 37;
         CHECK(chunk.tileAt(7, 3) == 37);
+    }
+
+    SECTION("Editing tiles") {
+        QPoint pos = { 7, 37 };
+        core::Chunk<qint32> chunk{ pos };
+
+        CHECK(chunk.tiles[9 * constants::_chunk_size + 6] == 0);
+        chunk.tileAt(6, 9) = 69;
+        CHECK(chunk.tiles[9 * constants::_chunk_size + 6] == 69);
+        CHECK(chunk.tileAt(6, 9) == 69);
+    }
+    
+    SECTION("Erasing tiles") {
+        QPoint pos = { 7, 37 };
+        core::Chunk<qint32> chunk{ pos };
+
+        chunk.tileAt(6, 9) = 69;
+        chunk.clearAt(6, 9);
+        CHECK(chunk.tileAt(6, 9) == 0);
+
+        for (qint32 x = 0; x < 4; ++x) {
+            for (qint32 y = 0; y < 4; ++y) {
+                chunk.tileAt(x, y) = 44;
+            }
+        }
+
+        chunk.clearArea(QRect{ QPoint{ 1, 1 }, QSize{ 2, 2 } });
+
+        for (qint32 x = 0; x < 4; ++x) {
+            for (qint32 y = 0; y < 4; ++y) {
+                if (x >= 1 && x <= 2 && y >= 1 && y <= 2)
+                    CHECK(chunk.tileAt(x, y) == 0);
+                else
+                    CHECK(chunk.tileAt(x, y) == 44);
+            }
+        }
     }
 }
--- vtLib/include/vt/core/chunk.hpp
+++ vtLib/include/vt/core/chunk.hpp
@@ -2,9 +2,10 @@
 
 #include <vt/util/constants.hpp>
 
-#include <QPoint>
+#include <QRect>
 
 #include <array>
+#include <cassert>
 
 namespace vt::core {
 
@@ -13,14 +14,38 @@ class Chunk {
 public:
     inline Chunk() = delete;
 
-    inline Chunk(quint16 x, quint16 y)
-        : id{ static_cast<quint32>(y * constants::_grid_size + x) }
+    inline Chunk(qint32 x, qint32 y)
+        : id{ y * constants::_grid_size + x }
+    {
+    }
+    inline Chunk(QPoint pos)
+        : Chunk{ pos.x(), pos.y() }
     {
     }
 
-    inline auto& tileAt(quint16 x, quint16 y) { return tiles[y * constants::_chunk_size + x]; }
-    inline const auto& tileAt(quint16 x, quint16 y) const { return tiles[y * constants::_chunk_size + x]; }
+    inline auto& tileAt(qint32 x, qint32 y) { return tiles[y * constants::_chunk_size + x]; }
+    inline auto& tileAt(QPoint pos) { return tileAt(pos.x(), pos.y()); }
+    inline const auto& tileAt(qint32 x, qint32 y) const { return tiles[y * constants::_chunk_size + x]; }
+    inline const auto& tileAt(QPoint pos) const { return tileAt(pos.x(), pos.y()); }
     inline auto getID() { return id; }
+
+    inline void clearAt(qint32 x, qint32 y) { tiles[y * constants::_chunk_size + x] = {}; }
+    inline void clearAt(QPoint pos) { clearAt(pos.x(), pos.y()); }
+    
+    void clearArea(qint32 x, qint32 y, qint32 width, qint32 height) {
+        assert((QRect{ 0, 0, constants::_chunk_size, constants::_chunk_size }.contains(QRect{ x, y, width, height })));
+        for (qint32 pos_x = x; pos_x < x + width; ++pos_x) {
+            for (qint32 pos_y = y; pos_y < y + height; ++pos_y) {
+                tileAt(pos_x, pos_y) = {};
+            }
+        }
+    }
+    inline void clearArea(QPoint pos, QSize size) {
+        clearArea(pos.x(), pos.y(), size.width(), size.height());
+    }
+    inline void clearArea(QRect rect) {
+        clearArea(rect.x(), rect.y(), rect.width(), rect.height());
+    }
 
 public:
     std::array<_TileT, constants::_chunk_size * constants::_chunk_size> tiles{};
