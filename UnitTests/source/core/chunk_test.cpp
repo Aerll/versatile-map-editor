af9226a05df99435134d58cc309c02b25bc1373c
Aerll <31746984+Aerll@users.noreply.github.com>
2022-07-14 20:41:20 +0200

    more functions for chunk

--- UnitTests/source/core/chunk_test.cpp
+++ UnitTests/source/core/chunk_test.cpp
@@ -17,17 +17,32 @@ TEST_CASE("core::Chunk") {
         CHECK(chunk.tileAt(7, 3) == 37);
     }
 
-    SECTION("Editing tiles") {
+    SECTION("Editing") {
         QPoint pos = { 7, 37 };
         core::Chunk<qint32> chunk{ pos };
 
         CHECK(chunk.tiles[9 * constants::_chunk_size + 6] == 0);
-        chunk.tileAt(6, 9) = 69;
+        chunk.tileAt({ 6, 9 }) = 69;
         CHECK(chunk.tiles[9 * constants::_chunk_size + 6] == 69);
-        CHECK(chunk.tileAt(6, 9) == 69);
+        CHECK(chunk.tileAt({ 6, 9 }) == 69);
+
+        CHECK(chunk.tileAt({ 3, 7 }) == 0);
+        chunk.replaceAt({ 3, 7 }, 37);
+        CHECK(chunk.tileAt({ 3, 7 }) == 37);
+
+        std::ranges::fill(chunk.tiles, 0);
+        chunk.replaceArea(QRect{ QPoint{ 1, 1 }, QSize{ 2, 3 } }, 37);
+        for (qint32 x = 0; x < 4; ++x) {
+            for (qint32 y = 0; y < 4; ++y) {
+                if (x >= 1 && x <= 2 && y >= 1 && y <= 3)
+                    CHECK(chunk.tileAt(x, y) == 37);
+                else
+                    CHECK(chunk.tileAt(x, y) == 0);
+            }
+        }
     }
     
-    SECTION("Erasing tiles") {
+    SECTION("Erasing") {
         QPoint pos = { 7, 37 };
         core::Chunk<qint32> chunk{ pos };
 
@@ -41,15 +56,59 @@ TEST_CASE("core::Chunk") {
             }
         }
 
-        chunk.clearArea(QRect{ QPoint{ 1, 1 }, QSize{ 2, 2 } });
+        chunk.clearArea(QRect{ QPoint{ 1, 1 }, QSize{ 2, 3 } });
 
         for (qint32 x = 0; x < 4; ++x) {
             for (qint32 y = 0; y < 4; ++y) {
-                if (x >= 1 && x <= 2 && y >= 1 && y <= 2)
+                if (x >= 1 && x <= 2 && y >= 1 && y <= 3)
                     CHECK(chunk.tileAt(x, y) == 0);
                 else
                     CHECK(chunk.tileAt(x, y) == 44);
             }
         }
+    }
+
+    SECTION("Search count") {
+        QPoint pos = { 7, 37 };
+        core::Chunk<qint32> chunk{ pos };
+
+        chunk.replaceArea(QRect{ QPoint{ 3, 3 }, QSize{ 7, 7 } }, 37);
+        auto found = chunk.findAll(37);
+        REQUIRE(found.size() == found.capacity());
+        REQUIRE(found.size() == 7 * 7);
+
+        found = chunk.findAll(0);
+        REQUIRE(found.size() == found.capacity());
+        REQUIRE(found.size() == chunk.tiles.size() - 7 * 7);
+
+        found = chunk.findAll(QRect{ QPoint{ 1, 1 }, QSize{ 4, 6 } }, 37);
+        REQUIRE(found.size() == found.capacity());
+        REQUIRE(found.size() == 2 * 4);
+    }
+
+    SECTION("Search result") {
+        QPoint pos = { 7, 37 };
+        core::Chunk<qint32> chunk{ pos };
+
+        chunk.tileAt(1, 1) = 69;
+        chunk.tileAt(3, 7) = 37;
+        chunk.tileAt(6, 9) = 69;
+
+        auto found = chunk.findAll(37);
+        REQUIRE(found.size() == 1);
+        CHECK(found[0] == QPoint{ 3, 7 });
+
+        found = chunk.findAll(69);
+        REQUIRE(found.size() == 2);
+        CHECK(found[0] == QPoint{ 1, 1 });
+        CHECK(found[1] == QPoint{ 6, 9 });
+
+        found = chunk.findAll(QRect{ QPoint{ 1, 1 }, QSize{ 3, 7 } }, 37);
+        REQUIRE(found.size() == 1);
+        CHECK(found[0] == QPoint{ 3, 7 });
+
+        found = chunk.findAll(QRect{ QPoint{ 1, 1 }, QSize{ 3, 7 } }, 69);
+        REQUIRE(found.size() == 1);
+        CHECK(found[0] == QPoint{ 1, 1 });
     }
 }
--- vtLib/include/vt/core/chunk.hpp
+++ vtLib/include/vt/core/chunk.hpp
@@ -30,6 +30,20 @@ public:
     [[nodiscard]] inline const auto& tileAt(qint32 x, qint32 y) const { return tiles[y * constants::_chunk_size + x]; }
     [[nodiscard]] inline const auto& tileAt(QPoint pos) const { return tileAt(pos.x(), pos.y()); }
 
+    inline void replaceAt(qint32 x, qint32 y, const _TileT& tile) { tileAt(x, y) = tile; }
+    inline void replaceAt(QPoint pos, const _TileT& tile) { replaceAt(pos.x(), pos.y(), tile); }
+
+    void replaceArea(qint32 x, qint32 y, qint32 width, qint32 height, const _TileT& tile) {
+        assert((QRect{ 0, 0, constants::_chunk_size, constants::_chunk_size }.contains(QRect{ x, y, width, height })));
+        for (qint32 pos_x = x; pos_x < x + width; ++pos_x) {
+            for (qint32 pos_y = y; pos_y < y + height; ++pos_y) {
+                tileAt(pos_x, pos_y) = tile;
+            }
+        }
+    }
+    inline void replaceArea(QPoint pos, QSize size, const _TileT& tile) { replaceArea(pos.x(), pos.y(), size.width(), size.height(), tile); }
+    inline void replaceArea(QRect rect, const _TileT& tile) { replaceArea(rect.x(), rect.y(), rect.width(), rect.height(), tile); }
+
     inline void clearAt(qint32 x, qint32 y) { tiles[y * constants::_chunk_size + x] = {}; }
     inline void clearAt(QPoint pos) { clearAt(pos.x(), pos.y()); }
     
@@ -41,12 +55,30 @@ public:
             }
         }
     }
-    inline void clearArea(QPoint pos, QSize size) {
-        clearArea(pos.x(), pos.y(), size.width(), size.height());
+    inline void clearArea(QPoint pos, QSize size) { clearArea(pos.x(), pos.y(), size.width(), size.height()); }
+    inline void clearArea(QRect rect) { clearArea(rect.x(), rect.y(), rect.width(), rect.height()); }
+
+    [[nodiscard]] auto findAll(qint32 x, qint32 y, qint32 width, qint32 height, const _TileT& tile) const {
+        assert((QRect{ 0, 0, constants::_chunk_size, constants::_chunk_size }.contains(QRect{ x, y, width, height })));
+        std::vector<QPoint> matched_tiles;
+
+        quint32 count = 0;
+        for (qint32 pos_x = x; pos_x < x + width; ++pos_x) {
+            for (qint32 pos_y = y; pos_y < y + height; ++pos_y)
+                count += tileAt(pos_x, pos_y) == tile;
+        }
+        matched_tiles.reserve(count);
+
+        for (qint32 pos_x = x; pos_x < x + width; ++pos_x) {
+            for (qint32 pos_y = y; pos_y < y + height; ++pos_y)
+                if (tileAt(pos_x, pos_y) == tile)
+                    matched_tiles.emplace_back(pos_x, pos_y);
+        }
+        return matched_tiles;
     }
-    inline void clearArea(QRect rect) {
-        clearArea(rect.x(), rect.y(), rect.width(), rect.height());
-    }
+    [[nodiscard]] inline auto findAll(QPoint pos, QSize size, const _TileT& tile) const { return findAll(pos.x(), pos.y(), size.width(), size.height(), tile); }
+    [[nodiscard]] inline auto findAll(QRect rect, const _TileT& tile) const { return findAll(rect.x(), rect.y(), rect.width(), rect.height(), tile); }
+    [[nodiscard]] inline auto findAll(const _TileT& tile) const { return findAll(0, 0, constants::_chunk_size, constants::_chunk_size, tile); }
 
 public:
     std::array<_TileT, constants::_chunk_size * constants::_chunk_size> tiles{};
